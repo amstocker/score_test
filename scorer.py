72343c57128dd9888ab9e939f29d9ae2bf2228b3
Andrew <Andrew Stocker>
2015-03-13 03:01:41 -0700

    first

---
+++ scorer.py
@@ -0,0 +1,101 @@
+import cPickle as pickle
+from copy import deepcopy
+from pprint import pprint
+
+import os
+import numpy as np
+from sys import argv
+from scraper import CommentLite
+
+class Node(object):
+  def __init__(self, comment):
+    self.comment = comment
+    self.score = 0.
+    self.leaf_count = 0
+    self.children = []
+
+
+class CommentTree(object):
+
+  def __init__(self, comments):
+    self._comments = deepcopy(comments)
+    self._tree = {}
+    self._root_id = self._comments[-1].id
+    self.populate()
+      
+     
+  def populate(self, prompt=False):
+    comments = deepcopy(self._comments)
+    
+    root = comments.pop()
+    self._tree[root.id] = Node(root)  # set thread root
+  
+    while len(comments)>0:
+      # clear terminal
+      if prompt: os.system('cls' if os.name == 'nt' else 'clear')
+      
+      com = comments.pop()
+      node = Node(com)
+      
+      self._tree[com.id] = node
+      self._tree[com.parent_id].children.append(node)
+      
+      # percolate leaf count if more than a comment chain
+      parent_id = com.parent_id
+      if self._tree[parent_id].leaf_count > 0:
+        while parent_id != None:
+          self._tree[parent_id].leaf_count += 1
+          parent_id = self._tree[parent_id].comment.parent_id
+      else:
+        self._tree[parent_id].leaf_count = 1
+      
+      self._calc_scores(com.created_utc)
+      
+      if prompt:
+        self.show()
+        raw_input("Enter to continue...")
+  
+  
+  def _calc_scores(self, current_utc):
+    for _id, node in self._tree.items():
+      if node.leaf_count == 0: continue
+      
+      dt = (current_utc - node.comment.created_utc) / 3600
+      node.score = np.exp( np.log( node.leaf_count ) / dt )
+  
+  
+  def show(self, hide_low=False):
+    node_fmt_str = "({})"
+    node_fmt = lambda n: node_fmt_str.format(n.score)
+    
+    def _helper(indent_level, root):
+      for child in root.children:
+        if hide_low and child.score>0: continue
+        
+        print indent_level*"\t" + node_fmt(child)
+        _helper(indent_level+1, child)
+    
+    if len(self._tree)>0: _helper(0, self._tree[self._root_id])
+    
+      
+
+
+
+if __name__=="__main__":
+  thread_id = argv[1]
+  pklfile = "{}.pkl".format(thread_id)
+  
+  comments = pickle.load( open(pklfile,'rb') )
+  
+  
+  tree = CommentTree(comments)
+  tree.show()
+  
+    
+    
+  
+  
+
+
+
+
---
+++ scraper.py
@@ -0,0 +1,74 @@
+import praw
+import cPickle as pickle
+
+from sys import argv
+
+
+
+# only keep necessary info for pickling
+class CommentLite(object):
+
+  def __init__(self, praw_comment_obj=None):
+    c = praw_comment_obj
+    
+    if c:
+      self.author = c.author.name
+      self.body = c.body
+      self.id = c.name
+      self.created_utc = c.created_utc
+      self.parent_id = c.parent_id
+    else:
+      self.author = None
+      self.body = None
+      self.id = None
+      self.created_utc = None
+      self.parent_id = None
+      
+    
+  def as_dict(self):
+    return self.__dict__
+  
+  @staticmethod
+  def fromthread(thread):
+    com = CommentLite()
+    com.author = 'THREAD_ROOT'
+    com.body = thread.title
+    com.id = thread.name
+    com.created_utc = thread.created_utc
+    return com
+
+
+### comment preparation pipeline ###
+
+# flatten comment tree
+flat = lambda comments: praw.helpers.flatten_tree(comments)
+
+# filter out MoreComments objects
+filt = lambda comments: [c for c in comments if c.__class__.__name__ != 'MoreComments']
+
+# convert praw comment objects to CommentLite objects
+lite = lambda comments: [CommentLite(c) for c in comments]
+
+# sort by time created (newest first)
+sort = lambda comments: sorted(comments, key=lambda c: -c.created_utc)
+
+# all of the above
+prep = lambda comments: sort(lite(filt(flat(comments))))
+
+
+
+if __name__=="__main__":
+  R = praw.Reddit('Testing for comment ranking algorithm research -- amstocker@dons.usfca.edu')
+  
+  thread_id = argv[1]
+  thread = R.get_submission(submission_id=thread_id)
+  
+  comments = prep(thread.comments)
+  comments = sort(comments + [CommentLite.fromthread(thread)])  # add thread root as comment
+  
+  pklfile = "{}.pkl".format(thread_id)
+  
+  print "dumping comments into {} ...".format(pklfile)
+  pickle.dump( comments, open(pklfile,'wb') )
+  
+  
